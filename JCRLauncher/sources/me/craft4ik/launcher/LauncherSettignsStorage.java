a5c8892c374faa2c90f39e429d2ef60fe10abc3c
sashok724 <sashok724@sashok724-PC>
2013-07-12 18:13:14 +0400

    Рефакторинг. '-' в имени проекта забыл.

--- JCRLauncher/sources/me/craft4ik/launcher/LauncherSettignsStorage.java
+++
@@ -1,7 +0,0 @@
-package me.craft4ik.launcher;
-
-public class LauncherSettignsStorage
-{
-	public static final String[] version_settings = new String[] { "qoobworld.ru v5.0", "13", "#2" };
-	public static final LauncherSettings settings = new LauncherSettingsImpl();
-}
--- JCRLauncher/sources/me/craft4ik/launcher/LauncherStarter.java
+++
@@ -1,9 +0,0 @@
-package me.craft4ik.launcher;
-
-public class LauncherStarter
-{
-	public static final void main(String[] var0) throws InterruptedException
-	{
-		Launcher.start();
-	}
-}
