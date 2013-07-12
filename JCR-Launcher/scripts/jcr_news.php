73d6054183d0510678add7c3612af182e966c352
sashok724 <sashok724@sashok724-PC>
2013-07-12 21:12:14 +0400

    Клинап!

--- JCR-Launcher/scripts/jcr_news.php
+++ JCR-Launcher/scripts/jcr_news.php
@@ -1,68 +1,67 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
-<head>
-<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE8">
-<meta http-equiv="Content-Language" content="ru">
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>�������</title>
-<style type="text/css">
-html,body {
-	font-family: Arial, Tahoma, Helvetica, sans-serif;
-	font-size: 9px;
-	color: <?php echo$_GET['color']; ?>;
-	margin: 0;
-}
-
-.title {
-	font-size: 10px;
-}
-
-.text {
-	font-size: 9px;
-}
-
-a {
-	color: <?php echo$_GET['color']; ?>;
-	text-decoration: none;
-}
-
-img {
-	border: 0;
-	margin: 0;
-}
-</style>
-</head>
-<body>
-  <?php
-		
-$file = fopen("news/news.txt", "r");
-		while(!feof($file))
-		{
-			$line = fgets($file);
-			$news = str_replace('[title]', '<b><span class="title">', $line);
-			$news = str_replace('[/title]', '</span></b><br />', $news);
-			$news = str_replace('[text]', '<span class="text">', $news);
-			$news = str_replace('[/text]', '</span>', $news);
-			$news = str_replace('[url="', '<a href="', $news);
-			$news = str_replace('[/url]', '</a>', $news);
-			$news = str_replace('"]', '">', $news);
-			$news = str_replace('[n]', '<div></div>', $news);
-			$news = str_replace(':)', '<img src="../news/data/smile.png"></img>', $news);
-			$news = str_replace(':(', '<img src="../news/data/frown.png"></img>', $news);
-			$news = str_replace('[rage]', '<img src="../news/data/mad.png"></img>', $news);
-			$news = str_replace('>:\\', '<img src="../news/data/mad.png"></img>', $news);
-			$news = str_replace(':\\', '<img src="../news/data/confused.png"></img>', $news);
-			$news = str_replace(':D', '<img src="../news/data/biggrin.png"></img>', $news);
-			$news = str_replace('B)', '<img src="../news/data/cool.png"></img>', $news);
-			$news = str_replace(':O', '<img src="../news/data/eek.png"></img>', $news);
-			$news = str_replace(':P', '<img src="../news/data/tongue.png"></img>', $news);
-			$news = str_replace(';D', '<img src="../news/data/wink.png"></img>', $news);
-			$news = str_replace('8)', '<img src="../news/data/rolleyes.png"></img>', $news);
-			$news = str_replace('[redface]', '<img src="../news/data/redface.png"></img>', $news);
-			$news = str_replace('[facepalm]', '<img src="../news/data/facepalm.gif"></img>', $news);
-			echo ($news);
-		}
-		fclose($file);
+	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE8">
+		<meta http-equiv="Content-Language" content="ru">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>Новости</title>
+		<style type="text/css">
+			html,body {
+				font-family: Arial, Tahoma, Helvetica, sans-serif;
+				font-size: 9px;
+				color: <?php echo$_GET['color']; ?>;
+				margin: 0;
+			}
+			
+			.title {
+				font-size: 10px;
+			}
+			
+			.text {
+				font-size: 9px;
+			}
+			
+			a {
+				color: <?php echo$_GET['color']; ?>;
+				text-decoration: none;
+			}
+			
+			img {
+				border: 0;
+				margin: 0;
+			}
+		</style>
+	</head>
+	<body>
+	  <?php
+			$file = fopen("news/news.txt", "r");
+			while(!feof($file))
+			{
+				$line = fgets($file);
+				$news = str_replace('[title]', '<b><span class="title">', $line);
+				$news = str_replace('[/title]', '</span></b><br />', $news);
+				$news = str_replace('[text]', '<span class="text">', $news);
+				$news = str_replace('[/text]', '</span>', $news);
+				$news = str_replace('[url="', '<a href="', $news);
+				$news = str_replace('[/url]', '</a>', $news);
+				$news = str_replace('"]', '">', $news);
+				$news = str_replace('[n]', '<div></div>', $news);
+				$news = str_replace(':)', '<img src="../news/data/smile.png"></img>', $news);
+				$news = str_replace(':(', '<img src="../news/data/frown.png"></img>', $news);
+				$news = str_replace('[rage]', '<img src="../news/data/mad.png"></img>', $news);
+				$news = str_replace('>:\\', '<img src="../news/data/mad.png"></img>', $news);
+				$news = str_replace(':\\', '<img src="../news/data/confused.png"></img>', $news);
+				$news = str_replace(':D', '<img src="../news/data/biggrin.png"></img>', $news);
+				$news = str_replace('B)', '<img src="../news/data/cool.png"></img>', $news);
+				$news = str_replace(':O', '<img src="../news/data/eek.png"></img>', $news);
+				$news = str_replace(':P', '<img src="../news/data/tongue.png"></img>', $news);
+				$news = str_replace(';D', '<img src="../news/data/wink.png"></img>', $news);
+				$news = str_replace('8)', '<img src="../news/data/rolleyes.png"></img>', $news);
+				$news = str_replace('[redface]', '<img src="../news/data/redface.png"></img>', $news);
+				$news = str_replace('[facepalm]', '<img src="../news/data/facepalm.gif"></img>', $news);
+				echo ($news);
+			}
+			fclose($file);
 		?>
 	</body>
 </html>
--- JCR-Launcher/scripts/jcr_status.php
+++ JCR-Launcher/scripts/jcr_status.php
@@ -1,75 +1,75 @@
 <?php
-define('IMPASS_CHECK', true);
-include ("jcr_settings.php");
-$action = $_GET ['action'];
-$getIP = $_GET ['ip'];
-$getPort = $_GET ['port'];
-if($action == 'status' && $getIP != null && $getPort != null)
-{
-	if(!$pauseMon)
+	define('IMPASS_CHECK', true);
+	include ("jcr_settings.php");
+	$action = $_GET ['action'];
+	$getIP = $_GET ['ip'];
+	$getPort = $_GET ['port'];
+	if($action == 'status' && $getIP != null && $getPort != null)
 	{
-		for($i = 0; $i < count($pauseSelMon); $i++)
+		if(!$pauseMon)
 		{
-			$splitServer = explode(" :: ", $pauseSelMon [$i]);
-			$contains = strCaseCmp($splitServer [0], $getIP) . strCaseCmp($splitServer [1], $getPort);
-			if($contains == '00') break;
-		}
-		if($contains != '00')
-		{
-			$Server = new ServerStatus($IP = $getIP, $Port = $getPort);
-			$status = $Server->Online ? true : false;
-			if($Server && $status)
+			for($i = 0; $i < count($pauseSelMon); $i++)
 			{
-				$players = preg_replace('/\D/', '', $Server->OnlinePlayers);
-				$maxplayers = preg_replace('/\D/', '', $Server->MaxPlayers);
-				if(empty($maxplayers)) echo 'TechWork';
-				else echo $players . '<::>' . $maxplayers;
+				$splitServer = explode(" :: ", $pauseSelMon [$i]);
+				$contains = strCaseCmp($splitServer [0], $getIP) . strCaseCmp($splitServer [1], $getPort);
+				if($contains == '00') break;
+			}
+			if($contains != '00')
+			{
+				$Server = new ServerStatus($IP = $getIP, $Port = $getPort);
+				$status = $Server->Online ? true : false;
+				if($Server && $status)
+				{
+					$players = preg_replace('/\D/', '', $Server->OnlinePlayers);
+					$maxplayers = preg_replace('/\D/', '', $Server->MaxPlayers);
+					if(empty($maxplayers)) echo 'TechWork';
+					else echo $players . '<::>' . $maxplayers;
+				} else
+					echo 'OFF';
 			} else
-				echo 'OFF';
+				echo 'TechWork';
 		} else
 			echo 'TechWork';
-	} else
-		echo 'TechWork';
-} else if($action == 'servers')
-{
-	for($i = 0; $i < count($servers); $i++)
+	} else if($action == 'servers')
 	{
-		$serversList = $serversList . $servers [$i] . '<::>' . '<br>';
+		for($i = 0; $i < count($servers); $i++)
+		{
+			$serversList = $serversList . $servers [$i] . '<::>' . '<br>';
+		}
+		echo substr_replace($serversList, '', strrpos($serversList, '<::>'));
 	}
-	echo substr_replace($serversList, '', strrpos($serversList, '<::>'));
-}
-class ServerStatus
-{
-	private $Socket, $Info;
-	public $Online, $OnlinePlayers, $MaxPlayers, $IP;
-
-	public function __construct($IP, $Port = '25565')
+	class ServerStatus
 	{
-		$Socket = @fsockopen($IP, $Port);
-		if($Socket == false)
+		private $Socket, $Info;
+		public $Online, $OnlinePlayers, $MaxPlayers, $IP;
+	
+		public function __construct($IP, $Port = '25565')
 		{
-			$this->Online = false;
-		} else
-		{
-			$this->Online = true;
-			fwrite($Socket, "\xFE");
-			$data = @fread($Socket, 256);
-			@fclose($Socket);
-			if($data == false or substr($data, 0, 1) != "\xFF") return;
-			$Info = substr($data, 3);
-			$Info = iconv('UTF-16BE', 'UTF-8', $Info);
-			if($Info [1] === "\xA7" && $Info [2] === "\x31")
+			$Socket = @fsockopen($IP, $Port);
+			if($Socket == false)
 			{
-				$Info = explode("\x00", $Info);
-				$this->OnlinePlayers = IntVal($Info [4]);
-				$this->MaxPlayers = IntVal($Info [5]);
+				$this->Online = false;
 			} else
 			{
-				$Info = Explode("\xA7", $Info);
-				$this->OnlinePlayers = IntVal($Info [1]);
-				$this->MaxPlayers = IntVal($Info [2]);
+				$this->Online = true;
+				fwrite($Socket, "\xFE");
+				$data = @fread($Socket, 256);
+				@fclose($Socket);
+				if($data == false or substr($data, 0, 1) != "\xFF") return;
+				$Info = substr($data, 3);
+				$Info = iconv('UTF-16BE', 'UTF-8', $Info);
+				if($Info [1] === "\xA7" && $Info [2] === "\x31")
+				{
+					$Info = explode("\x00", $Info);
+					$this->OnlinePlayers = IntVal($Info [4]);
+					$this->MaxPlayers = IntVal($Info [5]);
+				} else
+				{
+					$Info = Explode("\xA7", $Info);
+					$this->OnlinePlayers = IntVal($Info [1]);
+					$this->MaxPlayers = IntVal($Info [2]);
+				}
 			}
 		}
 	}
-}
 ?>
